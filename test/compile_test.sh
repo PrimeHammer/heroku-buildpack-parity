aee6f268c3f8fe441f66a17d667a2359c037a19f
Matej Minarik <mato.minarik@azet.sk>
2017-11-02 11:20:05 +0100

    Explicitly setup testing environment

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-echo "Build dir: ${BUILD_DIR}"
-../bin/compile ${BUILD_DIR}
+OUTPUT_DIR="$(mktemp -d ${SHUNIT_TMPDIR}/output.XXXX)"
+BUILD_DIR="${OUTPUT_DIR}/build"
+CACHE_DIR="${OUTPUT_DIR}/cache"
 
+mkdir -p ${OUTPUT_DIR}
+mkdir -p ${BUILD_DIR}
+mkdir -p ${CACHE_DIR}
+
+echo "Build dir: ${BUILD_DIR}"
+${BUILDPACK_HOME}/bin/compile ${BUILD_DIR} ${CACHE_DIR}
+
+rm -rf ${OUTPUT_DIR}
